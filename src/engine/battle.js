351f8f62aacb14bb73d6e424989423d5bf1fca7e
Romulo Martins <romulo_martins@id.uff.br>
2022-11-06 19:40:49 -0300

    Made speed process functions

--- src/engine/battle.js
+++ src/engine/battle.js
@@ -4,8 +4,32 @@ function getLog() {
     return log
 }
 
+function generateRandomSpeed(playerSpeed) {
+    return Math.floor(Math.random() * (playerSpeed + 1))
+}
+
+function getFirstPlayer(player1, player2) {
+    const player1Speed = generateRandomSpeed(player1.getSpeed())    
+    const player2Speed = generateRandomSpeed(player2.getSpeed())
+    
+    if (player1Speed == player2Speed) {
+        return getFirstPlayer(player1, player2)
+    } 
+
+    if (player1Speed > player2Speed) {
+        return player1
+    } else {
+        return player2
+    }
+}
+
 function run(player1, player2) {
     // TODO: implement game rules
 }
 
-module.exports = { run, getLog }
+module.exports = { 
+    run, 
+    getLog,
+    generateRandomSpeed,
+    getFirstPlayer
+}
--- test/engine/battle.test.js
+++ test/engine/battle.test.js
@@ -1,6 +1,40 @@
 const battleEngine = require('../../src/engine/battle')
+const Character = require('../../src/models/character')
 
-test('Before battle the log should be empty', () => {
-    expect(battleEngine.getLog()).toEqual([])
-});
+describe('On initial state', () => {
+    test('Before battle the log should be empty', () => {
+        expect(battleEngine.getLog()).toEqual([])
+    })
+})
 
+describe('generateRandomSpeed', () => {
+    test('should be a random number less or equal 9', () => {
+        expect(battleEngine.generateRandomSpeed(9)).toBeLessThanOrEqual(9)
+    })
+
+    test('should be a random number less or equal 5', () => {
+        expect(battleEngine.generateRandomSpeed(5)).toBeLessThanOrEqual(9)
+    })
+})
+
+describe('getFirstPlayer', () => {
+    test('player1 should start when this speed is greater', () => {
+        const player1 = new Character('Rei_Lagarto', 'Warrior')
+        player1.getSpeed = () => 5
+
+        const player2 = new Character('Legodas', 'Thief')
+        player2.getSpeed = () => 2
+
+        expect(battleEngine.getFirstPlayer(player1, player2)).toEqual(player1)
+    })
+
+    test('player1 should start when this speed is greater', () => {
+        const player1 = new Character('Rei_Lagarto', 'Warrior')
+        player1.getSpeed = () => 2
+
+        const player2 = new Character('Legodas', 'Thief')
+        player2.getSpeed = () => 5
+
+        expect(battleEngine.getFirstPlayer(player1, player2)).toEqual(player2)
+    })
+})
